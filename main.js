0060f0887b4ee1d6545fd235bdad1b32735ffe50
Mochaharpy <166887671+Mochaharpy@users.noreply.github.com>
2024-09-27 19:06:08 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: __dirname + '/preload.js'
+        }
+    });
+
+    win.loadURL('https://discord.com'); // Load Discord or any other URL
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
